f156bbcf3efaf86095878fedd2980ba6d1e3683c
Autumn Ragland <Autumn.Ragland@ipaper.com>
2019-09-24 10:33:22 -0500

    complete module 7 on adding features to components

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacePipe } from './shared/convert-to-spaces.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacePipe,
   ],
   imports: [
     BrowserModule,
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle = 'Product List';
     showImage = false;
-    listFilter = 'cart';
-    products = [
+    // listFilter = 'cart';
+    _listFilter: string;
+    get listFilter() {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.preformFilter(this.listFilter) : this.products;
+    }
+    filteredProducts: IProduct[];
+    products: IProduct[] = [
         {
             productId: 1,
             productName: 'Leaf Rake',
@@ -40,7 +51,19 @@ export class ProductListComponent {
             imageUrl: 'assets/images/hammer.png'
         },
     ];
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
+    }
+    preformFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+    ngOnInit(): void {
+        console.log('In OnInit');
     }
 }

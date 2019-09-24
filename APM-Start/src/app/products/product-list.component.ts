ca83bb2f6ad3cd508028edadb59601887d08613e
Autumn Ragland <Autumn.Ragland@ipaper.com>
2019-09-24 14:30:03 -0500

    complete module 9 - create and use a service

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -19,41 +20,9 @@ export class ProductListComponent implements OnInit {
         this.filteredProducts = this.listFilter ? this.preformFilter(this.listFilter) : this.products;
     }
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            productId: 1,
-            productName: 'Leaf Rake',
-            productCode: 'GDN-0011',
-            releaseDate: 'March 19, 2019',
-            description: 'Leaf rake with 48-inch wooden handle.',
-            price: 19.95,
-            starRating: 3.2,
-            imageUrl: 'assets/images/leaf_rake.png'
-        },
-        {
-            productId: 2,
-            productName: 'Garden Cart',
-            productCode: 'GDN-0023',
-            releaseDate: 'March 18, 2019',
-            description: '15 gallon capacity rolling garden cart',
-            price: 32.99,
-            starRating: 4.2,
-            imageUrl: 'assets/images/garden_cart.png'
-        },
-        {
-            productId: 5,
-            productName: 'Hammer',
-            productCode: 'TBX-0048',
-            releaseDate: 'May 21, 2019',
-            description: 'Curved claw steel hammer',
-            price: 8.9,
-            starRating: 4.8,
-            imageUrl: 'assets/images/hammer.png'
-        },
-    ];
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    products: IProduct[] = [];
+    constructor(private productService: ProductService) {
+        // this.filteredProducts = this.products;
     }
     onRatingClicked(message: string): void {
         this.pageTitle = 'Product List : ' + message;
@@ -67,6 +36,7 @@ export class ProductListComponent implements OnInit {
         this.showImage = !this.showImage;
     }
     ngOnInit(): void {
-        console.log('In OnInit');
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
 }

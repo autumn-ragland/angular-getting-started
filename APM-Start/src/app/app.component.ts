b1f2c4778e75f5e0945d050eca241d251c830c38
Autumn Ragland <Autumn.Ragland@ipaper.com>
2019-09-24 08:53:10 -0500

    Angular Getting Started tutroial module 1 - 7

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  //   template: `
+  //   <div>
+  //   <h1>{{title}}</h1>
+  //   <pm-produts></pm-products>
+  //   </div>
+  //   `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  title = 'Acme Product Managment';
 }

7daf33d846457ee687ba7519a40a19b6d0552455
Autumn Ragland <autumn.ragland@ipaper.com>
2019-09-25 17:05:11 -0500

    completed product sample app

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,31 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
+
 
 @Component({
-  selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle = 'Product Deatil';
-  product: IProduct;
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  pageTitle = 'Product Detail';
+  errorMessage = '';
+  product: IProduct | undefined;
+
+  constructor(private route: ActivatedRoute, private router: Router, private productService: ProductService) {
+  }
 
   ngOnInit() {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.pageTitle += `: ${id}`;
-    this.product = {
-      productId: id,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2019',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png',
-    };
+    const param = this.route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
+    }
   }
+
+  getProduct(id: number) {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
+  }
+
   onBack(): void {
     this.router.navigate(['/products']);
   }
